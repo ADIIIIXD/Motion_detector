c4358385a9e9549ff685c306ca4799254fb719fe
aditya <addie4120@gmail.com>
2023-08-08 12:36:54 +0530

    created emailing function and connected it to main

---
+++ emailing.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.message import EmailMessage
+import imghdr
+
+SENDER = "tbrown101299@gmail.com"
+PASSWORD = "oliltynrmjleqpjq"
+RECEIVER = "vikaditya119@gmail.com"
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New Object detected"
+    email_message.set_content("Hey, we just saw someone entering!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email(image_path="images/76.png")
